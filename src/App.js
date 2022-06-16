7997aa970ce3fab8ae9d47a6020d2d18395b1245
Cesar Centurion <ccenturion@ad-cap.com.ar>
2022-06-15 21:28:19 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -7,16 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Esto va a ser una tienda de herramientas!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
