40c24ca01e1d1eda160db48e3a9c9b2a63e1d258
Cesar Centurion <ccenturion@ad-cap.com.ar>
2022-07-18 23:25:33 -0300

    conditional rendering added

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -7,16 +7,19 @@ export const Cart = () => {
 
     return (
         <div className="container pt-4">
-            {cartContext?.cart?.addedItems.map(
+            { cartContext?.cart?.addedItems.length > 0? cartContext?.cart?.addedItems.map(
                 (prod, i) =>
                     // <h6 key={i}>{e.title} x {e.cantidad}  <button className=" btn btn-danger" >X</button> </h6>
                     <div className="row justify-content-center" key={i}>
                         <div className="col-4">{prod.title} x {prod.cantidad}</div>
                         <div className="col-2">
-                            <button onClick={()=>remove(prod)} className=" btn btn-danger" >X</button>
+                            <button onClick={()=>remove(prod)} className=" btn btn-danger">X</button>
                         </div>
                     </div>
-            )}
+            )
+                : <div> <h1>Tu carrito esta vacio!</h1> </div>
+        
+        }
         </div>
     )
 }
